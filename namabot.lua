a39e589b1a0fbc6b59b9d0a089706f924b0d2105
ditampol-tzuyoon <72374101+ditampol-tzuyoon@users.noreply.github.com>
2022-10-26 02:31:09 +0700

    Update namabot.lua

--- namabot.lua
+++ namabot.lua
@@ -1,16 +1,25 @@
 Rdp = {
     {nordp = 1, botname = "Ohdear", Status = "Nonaktif"},
+    {nordp = 1, botname = "OdStillAlive", Status = "Nonaktif"},
+    {nordp = 2, botname = "OdPayments", Status = "Nonaktif"},
     {nordp = 2, botname = "OdStillAlive", Status = "Nonaktif"},
     {nordp = 3, botname = "OdPayments", Status = "Nonaktif"},
+    {nordp = 3, botname = "OdStillAlive", Status = "Nonaktif"},
+    {nordp = 4, botname = "OdPayments", Status = "Nonaktif"},
     {nordp = 4, botname = "OdStillAlive", Status = "Nonaktif"},
     {nordp = 5, botname = "OdPayments", Status = "Nonaktif"},
-    {nordp = 6, botname = "OdStillAlive", Status = "Nonaktif"},
-    {nordp = 7, botname = "OdPayments", Status = "Nonaktif"},
-    {nordp = 8, botname = "OdStillAlive", Status = "Nonaktif"},
-    {nordp = 9, botname = "OdPayments", Status = "Nonaktif"},
-    {nordp = 10, botname = "OdStillAlive", Status = "Nonaktif"},
-    {nordp = 11, botname = "OdPayments", Status = "Nonaktif"}
+    {nordp = 5, botname = "OdStillAlive", Status = "Nonaktif"},
+    {nordp = 6, botname = "OdPayments", Status = "Nonaktif"}
+}
+
+ip_add = {
+    "192.168.0.1",
+    "192.168.0.2",
+    "192.168.0.3",
+    "192.168.0.4",
+    "192.168.0.5",
+    "192.168.0.6",
+    "192.168.0.7"
 }
 
 menit = 0.2
-TotalRdp = 11
