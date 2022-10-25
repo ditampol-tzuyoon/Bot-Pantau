8956791f0f7457718e94fbe54da0b3543b83b634
ditampol-tzuyoon <72374101+ditampol-tzuyoon@users.noreply.github.com>
2022-10-26 02:04:47 +0700

    Update namabot.lua

--- namabot.lua
+++ namabot.lua
@@ -1,7 +1,15 @@
 Rdp = {
     {nordp = 1, botname = "Ohdear", Status = "Nonaktif"},
     {nordp = 1, botname = "OdStillAlive", Status = "Nonaktif"},
-    {nordp = 2, botname = "OdPayments", Status = "Nonaktif"}
+    {nordp = 2, botname = "OdPayments", Status = "Nonaktif"},
+    {nordp = 2, botname = "OdStillAlive", Status = "Nonaktif"},
+    {nordp = 3, botname = "OdPayments", Status = "Nonaktif"},
+    {nordp = 3, botname = "OdStillAlive", Status = "Nonaktif"},
+    {nordp = 4, botname = "OdPayments", Status = "Nonaktif"},
+    {nordp = 4, botname = "OdStillAlive", Status = "Nonaktif"},
+    {nordp = 5, botname = "OdPayments", Status = "Nonaktif"},
+    {nordp = 5, botname = "OdStillAlive", Status = "Nonaktif"},
+    {nordp = 6, botname = "OdPayments", Status = "Nonaktif"}
 }
 
 menit = 0.2
